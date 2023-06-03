ca18c86fe61d8af703529c16098e7af229819ba8
luche83 <lucianachequer@gmail.com>
2023-06-03 12:32:45 -0300

    "cambios index agregar filtrar"

--- productos/index.js
+++ productos/index.js
@@ -4,8 +4,8 @@ const Producto = require('./Producto');
 module.exports = {
   productos: leerJSON(),
     listar : require('./listar'),
-    filtrar :  require('./filtar'),
-    buscar :  require('./agregar'),
+    agregar: require("./agregar"),
+    filtrar: require("./filtrar"),
 
   //   agregar:  require('./agregar'),
   //   filtrar :  require('./filtar'),
