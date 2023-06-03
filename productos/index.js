ec09e0857223acb04757c67fb3dde2290f7527c1
luche83 <lucianachequer@gmail.com>
2023-06-03 12:00:15 -0300

    "cambios lu filtrar buscar index"

--- productos/index.js
+++ productos/index.js
@@ -4,8 +4,11 @@ const Producto = require('./Producto');
 module.exports = {
   productos: leerJSON(),
     listar : require('./listar'),
+    filtrar :  require('./filtar'),
+    buscar :  require('./buscar'),
+
   //   agregar:  require('./agregar'),
   //   filtrar :  require('./filtar'),
   //   editar : require('./editar'),
-  //   buscar :  require('./buscar')
+  
 };
